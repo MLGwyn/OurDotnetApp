c57d06a8f43a2c829bc0e037c7e1df989747ed9a
Melissa Gwyn <melissagwyn@gmail.com>
2023-02-02 19:26:57 -0500

    intro to c#

--- Program.cs
+++ Program.cs
@@ -2,11 +2,15 @@
 
 namespace OurDotnetApp
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I was just created!");
+            Console.WriteLine("What's your name? ");
+            var name = Console.ReadLine();
+            Console.WriteLine($"It is neat to meet you {name}, " + "I am Melissa's Creation! ");
+
+        }
     }
-  }
 }
